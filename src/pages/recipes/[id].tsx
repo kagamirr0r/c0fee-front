19e58ff0a0a4e246a2be9083f1eb5fadcb1ba9f4
Kagamirr0r <a7112069@gmail.com>
2022-09-04 00:51:59 +0900

    型設定

--- src/pages/recipes/[id].tsx
+++ src/pages/recipes/[id].tsx
@@ -2,23 +2,48 @@ import type { NextPage } from 'next';
 import getRecipe from '@/api/getRecipe';
 import { useRouter } from 'next/router';
 
+type Recipe = {
+  hot_ice: string;
+  grind: string;
+  temperature: number;
+  bean_amount: number;
+  extracted_amount: number;
+  extraction: string;
+  recipe_image: { url: string };
+};
+
 const StatusPage: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
-  const { data, error } = getRecipe(id);
 
-  if (error) return <div>An error has occurred.</div>;
-  if (!data) return <div>Loading...</div>;
+  let errorData: any = '';
+  let recipe: Recipe = {
+    hot_ice: '',
+    grind: '',
+    temperature: 0,
+    bean_amount: 0,
+    extracted_amount: 0,
+    extraction: '',
+    recipe_image: { url: '' },
+  };
+
+  if (typeof id === 'string') {
+    const { data, error } = getRecipe(id);
+    errorData = error;
+    recipe = data;
+  }
+
+  if (errorData) return <div>An error has occurred.</div>;
+  if (!recipe) return <div>Loading...</div>;
   return (
     <>
-      <div className='mb-2 text-xl font-bold'>{data.name}</div>
-      <li> {data.hot_ice}</li>
-      <li> 挽き方: {data.grind}</li>
-      <li> 湯温: {data.temperature} ℃</li>
-      <li> 豆の量: {data.bean_amount}</li>
-      <li> 抽出量{data.extracted_amount}</li>
-      <li> 抽出方法{data.extraction}</li>
-      <img src={data.recipe_image.url} />
+      <div className='mb-2 text-xl font-bold'>{recipe.extraction}</div>
+      <li> {recipe.hot_ice}</li>
+      <li> 挽き方: {recipe.grind}</li>
+      <li> 湯温: {recipe.temperature} ℃</li>
+      <li> 豆の量: {recipe.bean_amount}</li>
+      <li> 抽出量{recipe.extracted_amount}</li>
+      <img src={recipe.recipe_image.url} />
     </>
   );
 };
--- src/pages/shops/[id].tsx
+++ src/pages/shops/[id].tsx
@@ -2,19 +2,39 @@ import type { NextPage } from 'next';
 import getShop from '@/api/getShop';
 import { useRouter } from 'next/router';
 
+type Shop = {
+  name: string;
+  url: string;
+  address: string;
+  shop_image: { url: string };
+};
+
 const StatusPage: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
-  const { data, error } = getShop(id);
 
-  if (error) return <div>An error has occurred.</div>;
-  if (!data) return <div>Loading...</div>;
+  let errorData: any = '';
+  let shop: Shop = {
+    name: '',
+    url: '',
+    address: '',
+    shop_image: { url: '' },
+  };
+
+  if (typeof id === 'string') {
+    const { data, error } = getShop(id);
+    errorData = error;
+    shop = data;
+  }
+
+  if (errorData) return <div>An error has occurred.</div>;
+  if (!shop) return <div>Loading...</div>;
   return (
     <>
-      <div className='mb-2 text-xl font-bold'>{data.name}</div>
-      <li> 住所: {data.address}</li>
-      <li> URL: {data.url}</li>
-      <img src={data.shop_image.url} />
+      <div className='mb-2 text-xl font-bold'>{shop.name}</div>
+      <li> 住所: {shop.address}</li>
+      <li> URL: {shop.url}</li>
+      <img src={shop.shop_image.url} />
     </>
   );
 };
